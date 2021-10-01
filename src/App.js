8d7b7d3ef00e06c963be1a386a7f76eecf13333c
Shubh1414 <sonawaneshubham140499@gmail.com>
2021-10-01 08:58:44 +0530

    React 1st Assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // State variables
+  const [number1, setNumber1] = useState();
+  const number2 = 20;
+  const [addition, setAddition] = useState();
+  const [subtraction, setSubtraction] = useState();
+
+  // Function to add numbers and update total in state
+  function add() {
+
+    setAddition(number1 + number2);
+  }
+  function sub() {
+
+    setSubtraction(number1 - number2);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+      <div className="App">
+        <div className="container">
+          <div className="container my-5 border-danger ">
+            <h3 className="my-5">-: Enter a Value to Add/Subtract with value 20 :-</h3>
+
+            <div className="number-inputs container my-5">
+              <input type="number" className="form-control border border-success" placeholder="Enter a Value" value={number1} onChange={e => setNumber1(+e.target.value)} />
+
+              <div className="input-group mb-3">
+                <button className="btn btn-outline-light mb-3 my-4 btn-info " onClick={add} type="button" id="button-addon1 btn ">Addition</button>
+                <input type="number" className="form-control mb-3 my-4 border border-warning rounded-pill" placeholder="" value={addition} aria-label="Example text with button addon" aria-describedby="button-addon1" />
+              </div>
+
+              <div className="input-group mb-3">
+                <button className="btn btn-outline-light mb-3 btn-warning  " onClick={sub} type="button" id="button-addon1 btn ">Subtraction</button>
+                <input type="number" className="form-control mb-3 border border-warning rounded-pill" placeholder="" value={subtraction} aria-label="Example text with button addon" aria-describedby="button-addon1" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
